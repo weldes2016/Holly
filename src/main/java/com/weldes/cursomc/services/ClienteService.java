67f34d1d00173c78426c6ab23472a4a29c34f972
Weldes Goncalves <weldes@miklus.com.br>
2019-06-27 13:55:37 -0300

    EndPoint Clientes{id}

---
+++ src/main/java/com/weldes/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.weldes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.weldes.cursomc.domain.Cliente;
+import com.weldes.cursomc.repositories.ClienteRepository;
+import com.weldes.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não Encontrado! Id:" +id +" ,Tipo: "+ Cliente.class.getName()));
+	}
+
+}
